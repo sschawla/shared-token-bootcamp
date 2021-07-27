5728a915ddabdc1e1f77a1ea015c9f27a83a4c4c
nutdanaiiam <natdanaiiamsamang@gmail.com>
2021-07-27 20:11:53 +0700

    add nai

--- src/test/java/bootcamp/StateTests.java
+++ src/test/java/bootcamp/StateTests.java
@@ -15,7 +15,7 @@ public class StateTests {
     private final Party bob = new TestIdentity(new CordaX500Name("Bob", "", "GB")).getParty();
     private final Party nut = new TestIdentity(new CordaX500Name("Nut", "", "TH")).getParty();
     private final Party ern = new TestIdentity(new CordaX500Name("Ern", "Bangkok", "TH")).getParty();
-
+    private final Party nai = new TestIdentity(new CordaX500Name("Nai", "Bangkok", "TH")).getParty();
 
 //    @Test
 //    public void tokenStateHasIssuerOwnerAndAmountParamsOfCorrectTypeInConstructor() {
