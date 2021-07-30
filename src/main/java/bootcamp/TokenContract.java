118ce607089d60967bbf405963f8e034a0d379ce
Nutnaree <NutnareeDeesittivate@hotmail.com>
2021-07-30 15:11:24 +0700

    Edit-ContractTestsAndTokenContract

--- src/main/java/bootcamp/TokenContract.java
+++ src/main/java/bootcamp/TokenContract.java
@@ -9,6 +9,7 @@ import net.corda.core.transactions.LedgerTransaction;
 import static net.corda.core.contracts.ContractsDSL.requireSingleCommand;    
 import static net.corda.core.contracts.ContractsDSL.requireThat;
 
+import java.security.PublicKey;
 import java.util.List;
 
 /* Our contract, governing how our state will evolve over time.
@@ -27,14 +28,21 @@ public class TokenContract implements Contract {
             CommandWithParties<Commands> command = requireSingleCommand(tx.getCommands(), Commands.class);
 
             if (command.getValue() instanceof TokenContract.Commands.Issue) {
-                 final TokenState outputTokenState =  tx.outputsOfType(TokenState.class).get(0);
+
 
                 requireThat (require -> {
                     require.using("No inputs should be consumed when issuing Token State.",tx.getInputStates().size()==0);
                     require.using("Only one outputs state is allowed.",tx.getOutputStates().size()==1);
-                    require.using("Wrong Output Type", outputTokenState instanceof TokenState);
+                    //require.using("Wrong Output Type", outputTokenState instanceof TokenState);
+                    require.using("Wrong Output Type", tx.getOutputStates().get(0) instanceof TokenState );
+
+                    final TokenState outputTokenState =  tx.outputsOfType(TokenState.class).get(0);
+                    final List<PublicKey> requiredSigners = command.getSigners();
+
                     require.using("Output Negative or Zero", outputTokenState.getAmount() > 0);
-                    require.using("Required signer is not an issuer",command.getSigners().contains(outputTokenState.getIssuer().getOwningKey()));
+                    //require.using("Required signer is not an issuer",command.getSigners().contains(outputTokenState.getIssuer().getOwningKey()));
+                    require.using("Required signer is not an issuer",requiredSigners.contains(outputTokenState.getIssuer().getOwningKey()));
+
 
 
 
